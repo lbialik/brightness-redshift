4f9c96026e5208bf925eafd3e30f1bb282f3e333
JackKenney <kenney.jnk@gmail.com>
2018-01-30 17:09:04 -0500

    fixed bugs, changed variable names to fit UPPER_CASE standard next step is to isolate out of main method into function call

--- light_adjust.py
+++ light_adjust.py
@@ -9,38 +9,48 @@ if __name__ == '__main__':
         print('Expected 2 arguments. Exiting.')
         sys.exit(1)
     # parse from storage file
-    try: 
+    try:
         with open('persistent_storage.json', 'r') as storage:
             SETTINGS = json.load(storage) # dictionary, {redshift:value, brightness:value}
     except FileNotFoundError:
-        with open('persistent_storage.json','w') as storage:
-            SETTINGS = {}
+        with open('persistent_storage.json', 'w') as storage:
+            SETTINGS = {
+                'brightness': 1,
+                'redshift': 6500
+            }
             json.dump(SETTINGS, storage)
-    if SETTINGS == {}:
-        SETTINGS = {
-            'brightness': 1,
-            'redshift': 6500
-        }
-    ARG = sys.argv[1] # direction
+    # direction
+    ARG = sys.argv[1]
     # current settings values
-    brightness = SETTINGS['brightness']
-    redshift = SETTINGS['redshift']
-    # determine flags (make adjustments)
+    BRIGHTNESS = SETTINGS['brightness']
+    REDSHIFT = SETTINGS['redshift']
+    # brightness range
+    B_MIN = 0.1
+    B_MAX = 1
+    B_DELTA = 0.05
+    # mins
+    R_MIN = 0
+    R_MAX = 10000
+    R_DELTA = 500
+    # determine FLAGS (make adjustments)
     if ARG == 'up':
-        brightness = 1 if (brightness + 0.1 > 1) else brightness + 0.1
+        BRIGHTNESS = B_MAX if (BRIGHTNESS + B_DELTA >
+                               B_MAX) else BRIGHTNESS + B_DELTA
     elif ARG == 'down':
-        brightness = 0.1 if (brightness - 0.1 < 0.1) else brightness-0.1
+        BRIGHTNESS = B_DELTA if (BRIGHTNESS - B_DELTA < B_DELTA) else BRIGHTNESS-B_DELTA
     elif ARG == 'right':
-        redshift = 13000 if (redshift + 500 > 13000) else redshift + 500
+        REDSHIFT = R_MAX if (REDSHIFT + R_DELTA >
+                             R_MAX) else REDSHIFT + R_DELTA
     elif ARG == 'left':
-        redshift = 0 if (redshift - 500 < 0) else redshift - 500
+        REDSHIFT = R_MIN if (REDSHIFT - R_DELTA <
+                             R_MIN) else REDSHIFT - R_DELTA
     # return settings values
-    SETTINGS['brightness'] = brightness
-    SETTINGS['redshift'] = redshift
+    SETTINGS['brightness'] = BRIGHTNESS
+    SETTINGS['redshift'] = REDSHIFT
     # default is no change to SETTINGS
-    flags = ['redshift', '-O', str(SETTINGS['redshift']), '-b', str(SETTINGS['brightness'])]
-    # make redshift call
-    run(flags)
+    FLAGS = ['redshift', '-O', str(SETTINGS['redshift']), '-b', str(SETTINGS['brightness'])]
+    # make system call
+    run(FLAGS)
     # write changes to storage
     with open('persistent_storage.json', 'w') as storage:
         json.dump(SETTINGS, storage)
